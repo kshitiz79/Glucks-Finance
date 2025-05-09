8fd61b1cac5e2757a048d407041221bc51119845
Kshitiz Kumar Maurya <kshitizkumarmaurya@MacBook-Pro.local>
2025-05-09 12:14:46 +0530

    d

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    
+      <p className="text-2xl">This is a sample Next.js application.</p>
+   </div>
+  );
+}
